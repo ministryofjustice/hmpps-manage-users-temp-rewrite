1e31af5804f2b344d85bc485e3be5cb5e38943b6
Mike Bostock <17684266+mpbostock@users.noreply.github.com>
2026-01-09 15:39:33 +0000

    HAAR-4703: add playwright tests for the menu page

--- integration_tests/pages/homePage.ts
+++ integration_tests/pages/homePage.ts
@@ -4,14 +4,41 @@ import AbstractPage from './abstractPage'
 export default class HomePage extends AbstractPage {
   readonly header: Locator
 
+  readonly noAdminFunctionsMessage: Locator
+
+  readonly bannerMessage: Locator
+
+  readonly tileList: Locator
+
   private constructor(page: Page) {
     super(page)
     this.header = page.locator('h1', { hasText: 'Manage user accounts' })
+    this.noAdminFunctionsMessage = page.getByTestId('no-admin-functions-message')
+    this.bannerMessage = page.getByTestId('banner-message')
+    this.tileList = page.getByTestId('tile-list')
   }
 
   static async verifyOnPage(page: Page): Promise<HomePage> {
     const homePage = new HomePage(page)
     await expect(homePage.header).toBeVisible()
     return homePage
+  }
+
+  async verifyTileCount(count: number): Promise<HomePage> {
+    const tileItems = this.tileList.getByRole('listitem')
+    await expect(tileItems).toHaveCount(count)
+    return this
+  }
+
+  async verifyTile(title: string, description: string, href: string, dataQa: string): Promise<HomePage> {
+    const tileLocator = this.page.getByTestId(dataQa)
+    const h2Locator = tileLocator.getByRole('heading', { level: 2 })
+    const linkLocator = tileLocator.getByRole('link')
+    const descriptionLocator = tileLocator.getByRole('paragraph')
+    await expect(linkLocator).toHaveAttribute('href', href)
+    await expect(h2Locator).toHaveText(title)
+    await expect(linkLocator).toHaveText(title)
+    await expect(descriptionLocator).toHaveText(description)
+    return this
   }
 }
