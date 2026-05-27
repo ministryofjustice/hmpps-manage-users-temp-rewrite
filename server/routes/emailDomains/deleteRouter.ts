3ed40ce03a0d13de0d60a9b3f8048d6f00c3ed8a
Mike Bostock <17684266+mpbostock@users.noreply.github.com>
2026-05-27 09:34:04 +0100

    HAAR-5430: addressing comments for improvements after testing (#20)

--- server/routes/emailDomains/deleteRouter.ts
+++ server/routes/emailDomains/deleteRouter.ts
@@ -6,9 +6,26 @@ import { EventType, SubjectType } from '../../services/auditService'
 import logger from '../../../logger'
 import authRoleGuardMiddleware from '../../middleware/route/authRoleGuardMiddleware'
 import AuthRole from '../../interfaces/authRole'
+import { bodyFromFlash, formErrorsFromFlash, validateFormOrRedirect } from '../../middleware/route/formMiddleware'
+import { FormError } from '../../interfaces/formError'
+
+interface Form {
+  confirmedDomain: string
+}
 
 type EmailDomainRequest = Request & {
   emailDomain: EmailDomain
+}
+
+const validate = (body: Form, req: EmailDomainRequest): FormError[] => {
+  const errors: FormError[] = []
+  const expectedDomain = req.emailDomain.domain
+
+  if (body.confirmedDomain !== expectedDomain) {
+    errors.push({ href: '#confirmedDomain', text: `Enter "${expectedDomain}" to confirm deletion of domain` })
+  }
+
+  return errors
 }
 
 export default (services: Services): Router => {
@@ -28,32 +45,42 @@ export default (services: Services): Router => {
   })
 
   router.get('/:id', async (req: EmailDomainRequest, res) => {
+    const body = bodyFromFlash<Form>(req)
+    const errors = formErrorsFromFlash(req)
     const { emailDomain } = req
     const deleteUrl = paths.emailDomains.deleteWithId({ id: emailDomain.id })
     const listUrl = paths.emailDomains.list.pattern
 
     return res.render('pages/emailDomains/delete', {
+      ...body,
       ...emailDomain,
       deleteUrl,
       listUrl,
+      errors,
     })
   })
 
-  router.post('/:id', async (req: EmailDomainRequest, res) => {
-    const { auditService, emailDomainsService } = services
-    const { emailDomain } = req
-    const { username } = res.locals.user
+  router.post(
+    '/:id',
+    validateFormOrRedirect(validate, (req: EmailDomainRequest) =>
+      paths.emailDomains.deleteWithId({ id: req.emailDomain.id }),
+    ),
+    async (req: EmailDomainRequest, res) => {
+      const { auditService, emailDomainsService } = services
+      const { emailDomain } = req
+      const { username } = res.locals.user
 
-    await emailDomainsService.deleteEmailDomain(res.locals.user.token, emailDomain.id)
-    await auditService.logAuditEvent({
-      what: EventType.DELETE_EMAIL_DOMAIN,
-      who: username,
-      subjectId: emailDomain.id,
-      subjectType: SubjectType.EMAIL_DOMAIN_ID,
-      details: emailDomain,
-    })
-    return res.redirect(paths.emailDomains.list.pattern)
-  })
+      await emailDomainsService.deleteEmailDomain(res.locals.user.token, emailDomain.id)
+      await auditService.logAuditEvent({
+        what: EventType.DELETE_EMAIL_DOMAIN,
+        who: username,
+        subjectId: emailDomain.id,
+        subjectType: SubjectType.EMAIL_DOMAIN_ID,
+        details: emailDomain,
+      })
+      return res.redirect(paths.emailDomains.list.pattern)
+    },
+  )
 
   return router
 }
