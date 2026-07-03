d754cf611245924709cadd2aced04042cc2c6d98
Mike Bostock <17684266+mpbostock@users.noreply.github.com>
2026-07-03 15:12:54 +0100

    Remove the example UI/API

--- server/services/index.ts
+++ server/services/index.ts
@@ -1,6 +1,5 @@
 import { dataAccess } from '../data'
 import AuditService from './auditService'
-import ExampleService from './exampleService'
 import MenuService from './menuService'
 import DpsUserService from './dpsUserService'
 import RolesService from './rolesService'
@@ -11,12 +10,11 @@ import ExternalUserService from './externalUserService'
 import paginationService from './paginationService'
 
 export const services = () => {
-  const { applicationInfo, hmppsAuditClient, exampleApiClient, manageUsersApiClient } = dataAccess()
+  const { applicationInfo, hmppsAuditClient, manageUsersApiClient } = dataAccess()
 
   return {
     applicationInfo,
     auditService: new AuditService(hmppsAuditClient),
-    exampleService: new ExampleService(exampleApiClient),
     menuService: new MenuService(manageUsersApiClient),
     dpsUserService: new DpsUserService(manageUsersApiClient),
     rolesService: new RolesService(manageUsersApiClient),
